ef96303ac909cf4e33ded7225d458de91120410e
Aliff <aliff.rosli96@gmail.com>
2021-09-30 18:17:31 +0800

    Mainpage

---
+++ resources/views/main.blade.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<head>
+  <!-- ... --->
+ <meta charset="UTF-8" />
+ <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<!-- ... --->
+<body>
+<link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
+<link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+    <section class="bg-auto pt-12 bg-transparent">
+        <div class="items-center flex flex-wrap">
+        <div class="w-full md:w-4/12 ml-auto mr-auto px-4">
+            <img class="max-w-full rounded-lg" src="image/me-modified.png">
+        </div>
+        <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
+            <div class="md:pr-12">
+            <div class="text-black-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-blue-300 mt-8">
+                <i class="fab fa-dev text-xl"></i>
+            </div>
+            <h3 class="text-3xl font-bold">MUHAMMAD ALIFF BIN ROSLI</h3>
+            <h4 class="text-2xl font-bold-italic">SOFTWARE ENGINEER</h4>
+            <p class="mt-4 text-lg leading-relaxed text-blueGray-500">
+                A tech enthusiast person with experienced PC Builder. Skilled in PHP, Java, HTML, Troubleshooting, and Bootstrap. Strong entrepreneurship professional with a Bachelor's degree focused in Computer Science from Universiti Teknologi MARA. 
+                Also an amateur e-sport player.
+            </p>
+            <ul class="list-none mt-6">
+                <li class="py-2">
+                <div class="flex items-center">
+                    <div>
+                    <span class="text-xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-black-600 bg-blue-300 mr-3"><i class="fas fa-code"></i></span>
+                    </div>
+                    <div>
+                    <h4 class="text-blueGray-500">
+                        (LAMP Stack)
+                    </h4>
+                    </div>
+                </div>
+                </li>
+                <li class="py-2">
+                <div class="flex items-center">
+                    <div>
+                    <span class="text-xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-black-600 bg-blue-300 mr-3"><i class="fab fa-laravel"></i></span>
+                    </div>
+                    <div>
+                    <h4 class="text-blueGray-500">Laravel</h4>
+                    </div>
+                </div>
+                </li>
+                <div class="flex items-center">
+                    <div>
+                    <span class="text-xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-black-600 bg-blue-300 mr-3"><i class="fab fa-github"></i></span>
+                    </div>
+                    <div>
+                    <h4 class="text-blueGray-500">Github</h4>
+                    </div>
+                </div>
+            </ul>
+            </div>
+        </div>
+        </div>
+    </section>
+    
+
+</body>
