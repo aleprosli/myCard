8121372e911803f96a8ab54d7e4532a8b8301e2e
Aliff <aliff.rosli96@gmail.com>
2021-10-01 01:11:29 +0800

    update content

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,6 +3,7 @@
   <!-- ... --->
  <meta charset="UTF-8" />
  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ <link rel="icon" href="{{ url('image/me-modified.png') }}">
  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <!-- ... --->
@@ -16,11 +17,14 @@
         </div>
         <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
             <div class="md:pr-12">
-            <div class="text-black-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-blue-300 mt-8">
-                <i class="fab fa-dev text-xl"></i>
+            <div class="flex flex-wrap justify-center gap-2">
+                <div class="text-black-600 text-center flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-blue-300 mt-8">
+                    <i class="fab fa-dev text-xl text-center"></i>
+                </div>
             </div>
             <h3 class="text-3xl font-bold">MUHAMMAD ALIFF BIN ROSLI</h3>
-            <h4 class="text-2xl font-bold-italic">SOFTWARE ENGINEER</h4>
+            <h4 class="text-2xl font-semibold">SOFTWARE ENGINEER</h4>
+            <h6 class="text-xl font-semibold">aliff.rosli96@gmail.com</h6>
             <p class="mt-4 text-lg leading-relaxed text-blueGray-500">
                 A tech enthusiast person with experienced PC Builder. Skilled in PHP, Java, HTML, Troubleshooting, and Bootstrap. Strong entrepreneurship professional with a Bachelor's degree focused in Computer Science from Universiti Teknologi MARA. 
                 Also an amateur e-sport player.
@@ -29,11 +33,11 @@
                 <li class="py-2">
                 <div class="flex items-center">
                     <div>
-                    <span class="text-xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-black-600 bg-blue-300 mr-3"><i class="fas fa-code"></i></span>
+                    <span class="text-xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-black-600 bg-blue-300 mr-3"><i class="fas fa-code text-xl"></i></span>
                     </div>
                     <div>
                     <h4 class="text-blueGray-500">
-                        (LAMP Stack)
+                        (LEMP Stack)
                     </h4>
                     </div>
                 </div>
@@ -41,7 +45,7 @@
                 <li class="py-2">
                 <div class="flex items-center">
                     <div>
-                    <span class="text-xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-black-600 bg-blue-300 mr-3"><i class="fab fa-laravel"></i></span>
+                    <span class="text-xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-black-600 bg-blue-300 mr-3"><i class="fab fa-laravel text-xl"></i></span>
                     </div>
                     <div>
                     <h4 class="text-blueGray-500">Laravel</h4>
@@ -50,13 +54,39 @@
                 </li>
                 <div class="flex items-center">
                     <div>
-                    <span class="text-xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-black-600 bg-blue-300 mr-3"><i class="fab fa-github"></i></span>
+                    <span class="text-xl font-semibold inline-block py-1 px-2 uppercase rounded-full text-black-600 bg-blue-300 mr-3"><i class="fab fa-github text-xl"></i></span>
                     </div>
                     <div>
                     <h4 class="text-blueGray-500">Github</h4>
                     </div>
                 </div>
             </ul>
+
+            <div>
+                <h3 class="text-3xl text-center font-semibold">SOCMED</h3>
+          
+                <div class="flex flex-wrap justify-center gap-2">
+                  <a href="https://www.facebook.com/aleprosli/" class="bg-blue-500 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                    <svg class="w-5 h-5 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" /></svg>
+                  </a>
+          
+                  <a href="https://www.linkedin.com/in/aleprosli/" class="bg-blue-600 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                    <svg class="w-5 h-5 fill-current" role="img" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+                      <g><path d="M218.123122,218.127392 L180.191928,218.127392 L180.191928,158.724263 C180.191928,144.559023 179.939053,126.323993 160.463756,126.323993 C140.707926,126.323993 137.685284,141.757585 137.685284,157.692986 L137.685284,218.123441 L99.7540894,218.123441 L99.7540894,95.9665207 L136.168036,95.9665207 L136.168036,112.660562 L136.677736,112.660562 C144.102746,99.9650027 157.908637,92.3824528 172.605689,92.9280076 C211.050535,92.9280076 218.138927,118.216023 218.138927,151.114151 L218.123122,218.127392 Z M56.9550587,79.2685282 C44.7981969,79.2707099 34.9413443,69.4171797 34.9391618,57.260052 C34.93698,45.1029244 44.7902948,35.2458562 56.9471566,35.2436736 C69.1040185,35.2414916 78.9608713,45.0950217 78.963054,57.2521493 C78.9641017,63.090208 76.6459976,68.6895714 72.5186979,72.8184433 C68.3913982,76.9473153 62.7929898,79.26748 56.9550587,79.2685282 M75.9206558,218.127392 L37.94995,218.127392 L37.94995,95.9665207 L75.9206558,95.9665207 L75.9206558,218.127392 Z M237.033403,0.0182577091 L18.8895249,0.0182577091 C8.57959469,-0.0980923971 0.124827038,8.16056231 -0.001,18.4706066 L-0.001,237.524091 C0.120519052,247.839103 8.57460631,256.105934 18.8895249,255.9977 L237.033403,255.9977 C247.368728,256.125818 255.855922,247.859464 255.999,237.524091 L255.999,18.4548016 C255.851624,8.12438979 247.363742,-0.133792868 237.033403,0.000790807055"></path></g>
+                    </svg>
+                    </a>
+          
+                  <a href="https://github.com/aleprosli" class="bg-gray-700 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                      <g fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z" fill="currentColor" /></g>
+                    </svg>
+                  </a>
+          
+                  <a href="mailto:aliff.rosli96@gmail.com" class="bg-red-500 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.372 0 0 5.373 0 12s5.372 12 12 12c6.627 0 12-5.373 12-12S18.627 0 12 0zm.14 19.018c-3.868 0-7-3.14-7-7.018c0-3.878 3.132-7.018 7-7.018c1.89 0 3.47.697 4.682 1.829l-1.974 1.978v-.004c-.735-.702-1.667-1.062-2.708-1.062c-2.31 0-4.187 1.956-4.187 4.273c0 2.315 1.877 4.277 4.187 4.277c2.096 0 3.522-1.202 3.816-2.852H12.14v-2.737h6.585c.088.47.135.96.135 1.474c0 4.01-2.677 6.86-6.72 6.86z" fill="currentColor"/></g></svg>
+                  </a>
+                </div>
+              </div>
             </div>
         </div>
         </div>
