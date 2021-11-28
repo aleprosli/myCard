68d18af643fa2aea2f89e80a641a6c632940b99a
Aliff <aliff.rosli96@gmail.com>
2021-11-29 00:06:44 +0800

    update blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,95 @@
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+<!-- component -->
+<section>
+	<div class="bg-black text-white py-20 md:flex h-full ">
+		<div class="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
+			<div class="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
+				<h1 class="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">Aliff Rosli</h1>
+				<h2 class="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">Software Engineer
+				</h2>
+				<p class="text-sm md:text-base text-gray-50 mb-4">A dedicated Software Engineer, self-learning tech enthusiast person with experienced PC Builder. Skilled in LEMP/LAMP Stack and Version Control (Git and Gitlab) with good Data-Structure, Object-Oriented Programming concept, and seek challenging problems with my Programming knowledge. Strong entrepreneurship professional with a Bachelor's degree focused in Computer Science from Universiti Teknologi MARA. Also an amateur e-sport player.</p>
+                <div class="shadow-md p-4 flex flex-row rounded-lg animate-bounce">
+                    <div class="bg-yellow-400 inline-block rounded-lg p-1 mr-1"></div>
+                    <b class="p-1">LAMP & LEMP Stack</b>
+                    <a class="h-5 w-5 text-gray-500 inline-block p-1">
+                    </a>
+                  </div>
+
+                  <div class="shadow-md p-4 flex flex-row rounded-lg animate-bounce">
+                    <div class="bg-yellow-400 inline-block rounded-lg p-1 mr-1"></div>
+                    <b class="p-1">Laravel</b>
+                    <a class="h-5 w-5 text-gray-500 inline-block p-1">
+                    </a>
+                  </div>
+
+                  <div class="shadow-md p-4 flex flex-row rounded-lg animate-bounce">
+                    <div class="bg-yellow-400 inline-block rounded-lg p-1 mr-1"></div>
+                    <b class="p-1">Git & Gitlab</b>
+                    <a class="h-5 w-5 text-gray-500 inline-block p-1">
+                    </a>
+                  </div>
+
+                  <div class="shadow-md p-4 flex flex-row rounded-lg animate-bounce">
+                    <div class="bg-yellow-400 inline-block rounded-lg p-1 mr-1"></div>
+                    <b class="p-1">PC Builder</b>
+                    <a class="h-5 w-5 text-gray-500 inline-block p-1">
+                    </a>
+                  </div>
+				<a href="https://drive.google.com/file/d/1sOzA4Q6E388W5YEIzwpVV6oLiMdOohOJ/preview"
+					class="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent">
+					Resume</a>
+                        
+                      
+			</div>
+			<div class="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
+				<div class="h-8 flex flex-wrap content-center">
+                    <div>
+                        <img class="inline-block mt-24 md:mt-0 p-8 md:p-0 "  src="{!! asset('image/1.png') !!}">
+                    </div>
+				</div>
+			</div>
+
+            <div>
+                <h2 class="text-3xl font-semibold text-center mb-6 text-gray-700">Social Media </h2>
+          
+                <div class="flex flex-wrap justify-center gap-2">
+                  <a href="https://www.facebook.com/aleprosli/" class="bg-blue-500 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                    <svg class="w-5 h-5 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" /></svg>
+                  </a>
+          
+                  <a href="https://www.linkedin.com/in/aleprosli/" class="bg-blue-600 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                    <svg class="w-5 h-5 fill-current" role="img" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+                      <g><path d="M218.123122,218.127392 L180.191928,218.127392 L180.191928,158.724263 C180.191928,144.559023 179.939053,126.323993 160.463756,126.323993 C140.707926,126.323993 137.685284,141.757585 137.685284,157.692986 L137.685284,218.123441 L99.7540894,218.123441 L99.7540894,95.9665207 L136.168036,95.9665207 L136.168036,112.660562 L136.677736,112.660562 C144.102746,99.9650027 157.908637,92.3824528 172.605689,92.9280076 C211.050535,92.9280076 218.138927,118.216023 218.138927,151.114151 L218.123122,218.127392 Z M56.9550587,79.2685282 C44.7981969,79.2707099 34.9413443,69.4171797 34.9391618,57.260052 C34.93698,45.1029244 44.7902948,35.2458562 56.9471566,35.2436736 C69.1040185,35.2414916 78.9608713,45.0950217 78.963054,57.2521493 C78.9641017,63.090208 76.6459976,68.6895714 72.5186979,72.8184433 C68.3913982,76.9473153 62.7929898,79.26748 56.9550587,79.2685282 M75.9206558,218.127392 L37.94995,218.127392 L37.94995,95.9665207 L75.9206558,95.9665207 L75.9206558,218.127392 Z M237.033403,0.0182577091 L18.8895249,0.0182577091 C8.57959469,-0.0980923971 0.124827038,8.16056231 -0.001,18.4706066 L-0.001,237.524091 C0.120519052,247.839103 8.57460631,256.105934 18.8895249,255.9977 L237.033403,255.9977 C247.368728,256.125818 255.855922,247.859464 255.999,237.524091 L255.999,18.4548016 C255.851624,8.12438979 247.363742,-0.133792868 237.033403,0.000790807055"></path></g>
+                    </svg>
+                </a>
+          
+                  <a href="https://github.com/aleprosli" class="bg-gray-700 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                      <g fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z" fill="currentColor" /></g>
+                    </svg>
+                </a>
+          
+                  <a href="mailto:aliff.rosli96@gmail.com" class="bg-red-500 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.372 0 0 5.373 0 12s5.372 12 12 12c6.627 0 12-5.373 12-12S18.627 0 12 0zm.14 19.018c-3.868 0-7-3.14-7-7.018c0-3.878 3.132-7.018 7-7.018c1.89 0 3.47.697 4.682 1.829l-1.974 1.978v-.004c-.735-.702-1.667-1.062-2.708-1.062c-2.31 0-4.187 1.956-4.187 4.273c0 2.315 1.877 4.277 4.187 4.277c2.096 0 3.522-1.202 3.816-2.852H12.14v-2.737h6.585c.088.47.135.96.135 1.474c0 4.01-2.677 6.86-6.72 6.86z" fill="currentColor"/></g></svg>
+                  </a>
+
+                  <a href="tel:+60135162634" class="bg-yellow-500 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />
+                      </svg>
+                  </a>
+                </div>
+              </div>
+		</div>
+    </div>
+    <div class="bg-black md:flex">
+        <footer class="flex flex-wrap items-center justify-between p-3 m-auto">
+            <div class="container mx-auto flex flex-col flex-wrap items-center justify-between">
+                <div class="flex mx-auto text-white text-center">
+                    Aliff Rosli © 2021
+                </div>
+            </div>
+        </footer>
+    </div>
+</section>
