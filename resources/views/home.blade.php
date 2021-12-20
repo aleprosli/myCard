3491e0cc5edfe9b03ca63fc7c1a48bad6bd87d30
aliff <aliff.rosli96@gmail.com>
2021-12-21 00:14:34 +0800

    update metatag

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,19 @@
 
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-
+<!doctype html>
+<head>
+  <!-- ... --->
+ <meta charset="UTF-8" />
+ <title>Aliff</title>
+  <meta name="description" content="free source">
+  <meta name="keywords" content="Laravel, Laravel Developer, Aliff, Rosli, Aliff Rosli,Muhammad Aliff Bin Rosli , Aliff Rosli Laravel Developer, Kedah, 
+  Selangor, PHP, Trainer Laravel, Laravel 8, Lamp Stack, Git workflow, Stack, Looka, Software Engineer">  
+ <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ <meta name="google-site-verification" content="0cA3VTdQH8c8XHkxMU5ESLjkz3Un6Imuu2yB_Sh1-jg" />
+ <link rel="icon" href="{{ url('image/me-modified.png') }}">
+ <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<!-- ... --->
+<body>
 <!-- component -->
 <section 
 >
